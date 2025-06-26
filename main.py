ffe2673ebee48b5b09372b52d4a1d158eb303bdf
anfeMurillo <anfehumu2.0@hotmail.com>
2025-06-26 16:11:54 -0500

    Agrego funcion inicio para comenzar el juego

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 if __name__ == "__main__":
-    print(
-        "Bienvenide a Aventura en la Mazmorra"
-        )
+
+    print("Bienvenide a la Aventura en la Mazmorra")
+    
+    def inicio ():
+        print("Quieres empezar? [si] [no]")
+        respuesta = input(">>> ")
+        if respuesta == "si":
+            ...
+        elif respuesta == "no":
+            print("Adios")
+        else:
+            print("Valor incorrecto, vuelve a intentarlo por favor")
+            inicio()
+    
+    inicio()
